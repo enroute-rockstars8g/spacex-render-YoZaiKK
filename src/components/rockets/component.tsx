11a8e2d144bbbdbe9a332d3594b40f2c928253a6
Guillermo <memocargo@hotmail.com>
2022-04-08 07:52:45 -0500

    Quitamos el types que sobraba y lineas que no se ocupaban, sigue funcionando

--- src/components/rockets/component.tsx
+++ src/components/rockets/component.tsx
@@ -1,15 +1,8 @@
-import React, { ChangeEvent, FC, useEffect, useState } from "react";
-// import {Dragon} from "../../problems.ts";
-// import { DragonDisplayProps } from "./types";
+import React, { FC, useEffect, useState } from "react";
 import { getRocketInfo } from "../../services/spaceX/service";
 
 export const RocketDisplay: FC = () => {
   const [rockets, setRockets] = useState<any>(undefined);
-  const [valor, setValor] = useState<number>(0); 
-  const funcion = () => {
-    setValor(valor + 1);
-    console.log("hola: ", rockets[0].name);
-  };  
 
   useEffect(() => {
     const firstDragon = async () => {
@@ -20,22 +13,18 @@ export const RocketDisplay: FC = () => {
   }, []);
   const imprimirCosas = (value: any) => {
     const paragraphs = [];
-    paragraphs.push(
-      <h1>
-        {value.name}
-      </h1>
-    );
+    paragraphs.push(<h1>{value.name}</h1>);
     return paragraphs;
   };
   return (
-    <div> 
+    <div>
       {rockets !== undefined ? (
         <div>
           {rockets.map((rocket: any) => (
             <p key={rocket.id}>
-              Nombre: {imprimirCosas(rocket)} {(rocket.description)}
+              {imprimirCosas(rocket)} {rocket.description}
             </p>
-          ))} 
+          ))}
         </div>
       ) : null}
     </div>
--- src/components/rockets/types.ts
+++
@@ -1,6 +0,0 @@
-import { ChangeEvent } from "react";
-
-export interface LaunchpadDisplayProps {
-    onChange: (event: ChangeEvent<HTMLInputElement>) => void;
-    position: number
-}
