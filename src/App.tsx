ee3e5006d78f6e624bee18b7d0044137685cd8cf
Guillermo <memocargo@hotmail.com>
2022-04-07 19:42:49 -0500

    Primer commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Esta no es una pagina de quimica
         </a>
       </header>
     </div>
